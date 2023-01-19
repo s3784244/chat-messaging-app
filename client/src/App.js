8007a4a5b069a51cb2e5d0c62099d2eaf691ea77
Kajal Soni <s3784244@student.rmit.edu.au>
2023-01-19 14:01:47 +1100

    Began implementing authentication

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { StreamChat } from 'stream-chat';
 import { ChannelList, Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
-import { ChannelListContainer, ChannelContainer } from './components';
+import { ChannelListContainer, ChannelContainer, Auth } from './components';
 import './App.css';
 
 // initialise the chat
@@ -11,7 +11,14 @@ const apiKey = '7m4tfvuh65c7';
 // create instance of stream chat
 const client = StreamChat.getInstance(apiKey);
 
+// only available once we have actually logged in
+const authToken = false;
+
 function App() {
+
+  if(!authToken) 
+    return <Auth />
+
   return (
     <div className="app__wrapper">
       <Chat client={client} theme="team light">
